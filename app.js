69a254c31447a5511f09917c5beb80781521d6fa
Alereza_RaDBlaCK01 <aliireza01ahmadi@gmail.com>
2024-02-10 03:49:29 +0100

    Getting Data

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-const BASE_URL =
-"https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=5dfdef68f9036cc5329e3fd2d4a26151&units=metric"
-const KEY_API = "5dfdef68f9036cc5329e3fd2d4a26151"
+const BASE_URL ="https://api.openweathermap.org/data/2.5"
+const API_KEY = "5dfdef68f9036cc5329e3fd2d4a26151"
+
+const searchInput = document.querySelector("input");
+const searchButton = document.querySelector("button");
+
+const getCurrentWeatherByName = async (city) => {
+    const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    const response = await fetch(url)
+    const json = await response.json();
+    return json
+}
+
+const searchHandler = async () => {
+    const cityName = searchInput.value;
+
+    if (!cityName) {
+        alert("plaese enter city name!");
+    }
+
+    const currentData = await getCurrentWeatherByName(cityName);
+    console.log(currentData);
+}
+
+searchButton.addEventListener("click" ,searchHandler)
